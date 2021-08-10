879fb1ef07352e013f2e00c503437b2bca4571e3
ashwith31 <ashwitash@gmail.com>
2021-08-10 08:54:30 +0530

    modified:Created an board to play the game

--- src/com/TicTacToe/TicTacToeGame.java
+++ src/com/TicTacToe/TicTacToeGame.java
@@ -1,11 +1,29 @@
 package com.TicTacToe;
 
+/*
+ *  Program to create a tictactoe game 
+ * @author Ashwith
+ */
+
 public class TicTacToeGame {
 
+	public static void gameboard()
+	{
+		char board[]=new char[10];
+		//method to give empty spaces to board array
+		for(int i=1;i<board.length;i++)
+		{
+			board[i]=' ';
+		}
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Tic-Tac-Toe game");
-	
+		//created an object 
+		TicTacToeGame player=new TicTacToeGame();
+		//calling the gameboard method using object
+		player.gameboard();                      
+		
 	} 
 	
 }
