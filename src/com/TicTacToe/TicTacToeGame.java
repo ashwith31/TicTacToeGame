32279b7be0daf3372b0fb2b50cbfeb94b7b028bc
ashwith31 <ashwitash@gmail.com>
2021-08-13 19:44:27 +0530

    modified:Ability of the player to select an option for gameplay

--- src/com/TicTacToe/TicTacToeGame.java
+++ src/com/TicTacToe/TicTacToeGame.java
@@ -1,29 +1,55 @@
 package com.TicTacToe;
 
+import java.util.Scanner;
+
 /*
- *  Program to create a tictactoe game 
+ *  Program to create a TicTacToe game 
  * @author Ashwith
  */
 
 public class TicTacToeGame {
-
-	public static void gameboard()
-	{
+	//variables 
+	static char playerOption,computerOption;
+	static Scanner sc=new Scanner(System.in);
+	/*
+	 * This is a function that is used to create a board for the gameplay.
+	 * We are initializing the board with empty spaces and
+	 * starting from index "1".
+	 * @return This returns a board array
+	 */
+	public static char[] createboard(){
 		char board[]=new char[10];
-		//method to give empty spaces to board array
-		for(int i=1;i<board.length;i++)
-		{
-			board[i]=' ';
+		for(int index=1;index<board.length;index++){
+			board[index]=' ';
+			}
+		return board;
+	}
+	/*
+	 * This is a method which allows the player to select either "X" or "O" as his/her option in the gameplay
+	 */
+	public static void chooseOption() {
+		System.out.println("Which option do you want to choose.");
+		System.out.println("1)'X' or 2)'O'");
+		char s=sc.next().charAt(0);
+		if(s=='X'){
+			playerOption='X';
+			computerOption='O';
 		}
+		else if(s=='O'){
+			playerOption='O';
+			computerOption='X';
+		}
+		else {
+			System.out.println("Invalid option");
+		}
+		System.out.println("OK.You have selected "+s);
 	}
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Tic-Tac-Toe game");
-		//created an object 
-		TicTacToeGame player=new TicTacToeGame();
-		//calling the gameboard method using object
-		player.gameboard();                      
-		
+		createboard();
+		chooseOption();
+		System.out.println(playerOption);
 	} 
 	
 }
