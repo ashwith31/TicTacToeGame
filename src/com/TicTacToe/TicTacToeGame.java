02d3594784094d7b37728d7f2748dd0a8690b7ee
ashwith31 <ashwitash@gmail.com>
2021-08-21 19:30:02 +0530

    modified:After every move the game checks for winner

--- src/com/TicTacToe/TicTacToeGame.java
+++ src/com/TicTacToe/TicTacToeGame.java
@@ -1,5 +1,6 @@
 package com.TicTacToe;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -10,6 +11,8 @@ import java.util.Scanner;
 public class TicTacToeGame {
 	// variables
 	static char playerOption, computerOption;
+	static int flag;
+	static boolean b = true;
 	// array to store values of player and computer
 	static char board[] = new char[10];
 	static Scanner sc = new Scanner(System.in);
@@ -64,9 +67,9 @@ public class TicTacToeGame {
 	 * This method is to create for user to make a move to a desired location in the
 	 * board if there is a free space in that location. Input will take input from
 	 * user like from 1 to 9. If it is not in range from 1 to 9. Then it will show
-	 * you an error "Invalid input."
+	 * you an error "Invalid move."
 	 */
-	public static void desiredLocation() {
+	public static void makeMovePlayer() {
 		boolean validinput = false;
 		do {
 			System.out.println("Enter the position you want to place the element");
@@ -82,7 +85,19 @@ public class TicTacToeGame {
 		} while (!validinput);
 	}
 
-	/**
+	public static void makeMoveComp() {
+		boolean c = false;
+		do {
+			int i = (int) Math.floor(Math.random() * 10);
+			if (board[i] == ' ' && board[i] != 'X' && board[i] != 'O') {
+				board[i] = computerOption;
+				c = true;
+			}
+		} while (!c);
+		showBoard();
+	}
+
+	/*
 	 * This method is to flip a coin who plays first select either 0 for Tail or 1
 	 * for Head
 	 * 
@@ -92,10 +107,104 @@ public class TicTacToeGame {
 		System.out.println("Select 0 for Tail or 1 for Head to see who plays first");
 		int playerSelection = sc.nextInt(); // player selecting the option either 0 or 1
 		int toss = (int) Math.floor(Math.random() * 10) % 2;
-		if (playerSelection == toss)
+		if (playerSelection == toss) {
 			System.out.println("Player Won the Toss and Plays First");
-		else
+			flag = 0;
+		} else {
 			System.out.println("Computer Won the Toss and Plays First");
+			flag = 1;
+		}
+	}
+
+	/*
+	 * This method actually initiates the gameplay.If the player wins the toss he
+	 * plays first else computer plays first.
+	 */
+	public static void startGame() {
+		if (flag == 0) {
+			while (b) {
+				makeMovePlayer();
+				checkWinnerPlayer();
+				makeMoveComp();
+				checkWinnerComputer();
+			}
+		} else {
+			while (b) {
+				makeMoveComp();
+				checkWinnerComputer();
+				makeMovePlayer();
+				checkWinnerPlayer();
+			}
+		}
+	}
+
+	/*
+	 * This method is used to check if the player wins the game.
+	 * 
+	 * @return This method returns a boolean data type
+	 */
+	public static boolean checkWinnerPlayer() {
+
+		if (board[1] == playerOption && board[2] == playerOption && board[3] == playerOption) {
+			System.out.println("You won");
+			b = false;
+		}
+		if (board[1] == playerOption && board[5] == playerOption && board[9] == playerOption) {
+			System.out.println("You won");
+			b = false;
+		}
+		if (board[1] == playerOption && board[4] == playerOption && board[7] == playerOption) {
+			System.out.println("You won");
+			b = false;
+		}
+		if (board[2] == playerOption && board[5] == playerOption && board[8] == playerOption) {
+			System.out.println("You won");
+			b = false;
+		}
+		if (board[3] == playerOption && board[6] == playerOption && board[9] == playerOption) {
+			System.out.println("You won");
+			b = false;
+		}
+		if (board[4] == playerOption && board[5] == playerOption && board[6] == playerOption) {
+			System.out.println("You won");
+			b = false;
+		}
+		return b;
+	}
+
+	/*
+	 * This method is used to check if the computer wins the game.
+	 * 
+	 * @return This method returns a boolean data type
+	 */
+	public static boolean checkWinnerComputer() {
+
+		if (board[1] == computerOption && board[2] == computerOption && board[3] == computerOption) {
+			System.out.println("Computer won");
+			b = false;
+		}
+		if (board[1] == computerOption && board[5] == computerOption && board[9] == computerOption) {
+			System.out.println("Computer won");
+			b = false;
+		}
+		if (board[1] == computerOption && board[4] == computerOption && board[7] == computerOption) {
+			System.out.println("Computer won");
+			b = false;
+		}
+		if (board[2] == computerOption && board[5] == computerOption && board[8] == computerOption) {
+			System.out.println("Computer won");
+			b = false;
+		}
+		if (board[3] == computerOption && board[6] == computerOption && board[9] == computerOption) {
+			System.out.println("Computer won");
+			b = false;
+		}
+		if (board[4] == computerOption && board[5] == computerOption && board[6] == computerOption) {
+			System.out.println("Computer won");
+			b = false;
+		}
+
+		return b;
 	}
 
 	public static void main(String[] args) {
@@ -103,8 +212,8 @@ public class TicTacToeGame {
 		System.out.println("Welcome to Tic-Tac-Toe game");
 		createboard();
 		chooseOption();
-		showBoard();
 		toss();
+		startGame();
 	}
 
 }
